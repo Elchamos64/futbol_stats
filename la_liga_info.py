c8246e97181b5124263219017ebbf2590406564e
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-05-10 16:09:18 -0600

    added more teams to the league

--- la_liga_info.py
+++ la_liga_info.py
@@ -6,7 +6,7 @@ from tabulate import tabulate
 def la_liga_standings():
     conn = sqlite3.connect('soccer_teams.db')
     c = conn.cursor()
-    c.execute('''SELECT * FROM teams''')
+    c.execute('''SELECT * FROM teams ORDER BY points DESC''')
     team = c.fetchall()
     conn.close()
     return team
@@ -16,7 +16,7 @@ def la_liga_standings():
 def lookup_team(name):
     conn = sqlite3.connect('soccer_teams.db')
     c = conn.cursor()
-    c.execute('''SELECT * FROM teams WHERE name=?''', (name,))
+    c.execute('''SELECT * FROM teams WHERE name=?''', (name))
     team = c.fetchone()
     conn.close()
     return team
@@ -25,13 +25,13 @@ def lookup_team(name):
 def main():
     done = False
     while not done :
-        print("1. Look up team information")
+        print("\n1. Look up team information")
         print("2. Standing La Liga info")
         print("3. Quit")
-        
+
         choice = input("Enter your choice (1/2/3): ")
-    
-        
+
+
         if choice == '1':
             name = input("Enter team name to look up: ")
             team = lookup_team(name)
@@ -44,21 +44,25 @@ def main():
                 print("Team not found.")
 
         elif choice == '2':
-            teams = la_liga_standings()
-            print("\nTeam Information:")
-            headers = ["Name", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
-            for team in teams:
-                data = [[team[1], team[2], team[3], team[4], team[5], team[6], team[7], team[8], team[9]]]
-                print(tabulate(data, headers=headers, tablefmt="grid"))
+          teams = la_liga_standings()
+          headers = ["Name", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
+          all_data = []
 
-        
+          for team in teams:
+            # Access elements from each inner list
+            data = [team[1], team[2], team[3], team[4], team[5], team[6], team[7], team[8], team[9]]
+            all_data.append(data)
+
+          print("\nTeam Information:")
+          print(tabulate(all_data, headers=headers, tablefmt="grid"))
+
+
         elif choice == '3':
             print("Exiting program.")
             done = True
-        
+
         else:
             print("Invalid choice. Please enter 1, or 2.")
 
 if __name__ == "__main__":
     main()
-
